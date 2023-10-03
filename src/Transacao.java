da7fca5f04d5faa99a972af9086e5f2a76263e5f
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-03 01:36:13 -0300

    Desenvolvendo a classe Transação

--- src/Transacao.java
+++ src/Transacao.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transacao {
-    
+    private List<String> transacoes;
+
+    public Transacao() {
+        this.transacoes = new ArrayList<>();
+    }
+
+    // Método para adicionar uma mensagem de transação
+    public void adicionarTransacao(String mensagem) {
+        transacoes.add(mensagem);
+    }
+
+    // Método para obter todas as transações
+    public List<String> getTransacoes() {
+        return transacoes;
+    }
+
+    // Método para limpar todas as transações
+    public void limparTransacoes() {
+        transacoes.clear();
+    }
 }
