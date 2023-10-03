308c96efb42684a88e06ebcaa0a02b45eeedabf4
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-03 01:38:41 -0300

    Lista de contas associadas ao cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
     private String nome;
     private String CPF;
     private String dataNascimento;
     private String endereco;
-    
+    private List<Conta> contas; // Lista de contas associadas ao cliente
 
     public Cliente(String nome, String CPF, String dataNascimento, String endereco){
         this.nome = nome;
         this.CPF = CPF;
         this.dataNascimento = dataNascimento;
         this.endereco = endereco;
-        
+        this.contas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -39,5 +42,13 @@ public class Cliente {
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
     }
 }
