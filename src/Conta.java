977584cc875c8e1faaca3cd511996ac59f4633ba
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-03 01:34:53 -0300

    Criando métodos de saque, depósito e transferência

--- src/Conta.java
+++ src/Conta.java
@@ -26,4 +26,31 @@ public class Conta {
     public Cliente getCliente() {
         return cliente;
     }
+
+    // Método para depositar dinheiro na conta
+    public void depositar(double valor){
+        saldo += valor;
+        System.out.println("Depósito concluído com êxito");
+    }
+
+    // Método para sacar dinheiro da conta
+    public void sacar(double valor){
+        if (valor <= saldo){
+            saldo -= valor;
+            System.out.println("Saque concluído com êxito");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    // Método para transferir dinheiro para outra conta
+    public void transferir(Conta destino, double valor){
+        if (valor <= saldo){
+            saldo -= valor;
+            destino.depositar(valor);
+            System.out.println("Transferência concluída com êxito");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
 }
