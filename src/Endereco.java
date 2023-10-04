3528d64a369e587dec53d4f74261d9cce0eff514
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-03 21:12:42 -0300

    Definindo atributos e o construtor da classe

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,3 +1,21 @@
-public Endereco {
-    
+public class Endereco {
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String CEP;
+
+    public Endereco(String rua, String numero, String complemento, String bairro, String cidade, String estado, String pais, String CEP){
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.CEP = CEP;
+    }
 }
