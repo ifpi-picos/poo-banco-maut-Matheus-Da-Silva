6ea607bf9eff8e59f6347b9421cda8afe374b224
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-21 08:49:57 -0300

    Desenvolvendo o polimorfismo dos métodos

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -6,5 +6,34 @@ public class ContaPoupanca extends Conta {
         this.rendimento = rendimento;
     }
 
-    
+    @Override
+    public double sacar(double valor) {
+        if (valor <= saldo) {
+            double taxa = valor * 0.05; // Taxa de 5% para saques
+            super.sacar(valor + taxa); 
+            return valor;
+        } else {
+            System.out.println("Saldo insuficiente");
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean depositar(double valor) {
+        double rendimentoValor = valor * rendimento;
+        double valorComRendimento = valor + rendimentoValor;
+        transacao.adicionarTransacao("Depósito com Rendimento", valorComRendimento, obterDataHoraFormatada());
+        return super.depositar(valorComRendimento);
+    }
+
+    @Override
+    public void transferir(Conta destino, double valor) {
+        if (valor <= saldo) {
+            double taxa = valor * 0.10; // Taxa de 10% para transferências
+            super.sacar(taxa);
+            destino.depositar(valor - taxa);
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
 }
