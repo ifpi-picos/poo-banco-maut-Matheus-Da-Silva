c0f14413be5f345be4c6b0790facb06a1353f889
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-21 08:47:41 -0300

    Desenvolvendo polimorfismo do método transferir

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -8,5 +8,24 @@ public class ContaCorrente extends Conta {
         this.transferenciasRestantes = 2; // Inicialmente, 2 transferências sem taxa
     }
 
-    
+    @Override
+    public void transferir(Conta destino, double valor) {
+        if (valor <= (saldo + chequeEspecial)) {
+            saldo -= valor;
+
+            if (transferenciasRestantes > 0) {
+                destino.depositar(valor); // Destino deve receber um depósito
+                transferenciasRestantes--;
+            } else {
+                double taxa = valor * 0.10; // Taxa de 10% para transferências adicionais
+                super.sacar(taxa);
+                destino.depositar(valor - taxa); // Desconta a taxa do destino
+            }
+            
+            transacao.adicionarTransacao("Transferência", valor, obterDataHoraFormatada());
+            System.out.println("Transferência concluída com êxito");
+        } else {
+            System.out.println("Saldo e cheque especial insuficientes");
+        }
+    }
 }
